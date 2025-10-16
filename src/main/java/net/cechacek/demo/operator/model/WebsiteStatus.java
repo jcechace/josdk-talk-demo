958a3b8f50434ff184a0a411f9fb9b09c067853e
Jakub Cechacek <jakub@cechacek.net>
2025-10-16 12:13:52 +0200

    Added status property descriptions

--- src/main/java/net/cechacek/demo/operator/model/WebsiteStatus.java
+++ src/main/java/net/cechacek/demo/operator/model/WebsiteStatus.java
@@ -1,11 +1,14 @@
 package net.cechacek.demo.operator.model;
 
 import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonPropertyDescription;
 
 @JsonInclude(JsonInclude.Include.NON_NULL)
 public class WebsiteStatus {
 
+    @JsonPropertyDescription("Whether website is ready")
     private boolean ready = false;
+    @JsonPropertyDescription("Error message if something goes wrong")
     private String errorMessage;
 
     public boolean isReady() {
