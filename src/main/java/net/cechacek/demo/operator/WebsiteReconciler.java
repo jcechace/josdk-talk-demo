4b7a72c1f0869ce2973c6b8801da4b138a477d94
Jakub Cechacek <jakub@cechacek.net>
2025-10-23 09:14:33 +0200

    Updates and build improvements

--- src/main/java/net/cechacek/demo/operator/WebsiteReconciler.java
+++ src/main/java/net/cechacek/demo/operator/WebsiteReconciler.java
@@ -1,17 +1,13 @@
 package net.cechacek.demo.operator;
 
-import io.fabric8.kubernetes.api.model.ObjectMetaBuilder;
-import io.javaoperatorsdk.operator.api.config.informer.Informer;
 import io.javaoperatorsdk.operator.api.reconciler.*;
 import io.javaoperatorsdk.operator.api.reconciler.dependent.Dependent;
-import io.javaoperatorsdk.operator.processing.dependent.workflow.WorkflowReconcileResult;
 import io.quarkus.logging.Log;
 import net.cechacek.demo.operator.conditions.HtmlValidCondition;
 import net.cechacek.demo.operator.dependents.ContentConfigMapDependent;
 import net.cechacek.demo.operator.dependents.HtmlPodDependent;
 import net.cechacek.demo.operator.dependents.ServiceDependent;
 import net.cechacek.demo.operator.model.Website;
-import net.cechacek.demo.operator.model.WebsiteStatus;
 
 import java.util.concurrent.TimeUnit;
 
@@ -22,22 +18,19 @@ import static net.cechacek.demo.operator.ResourceUtils.createWebsiteForStatusUpd
         @Dependent(type = HtmlPodDependent.class, dependsOn = "content"),
         @Dependent(type = ServiceDependent.class)
 })
-@ControllerConfiguration(
-        informer = @Informer(namespaces = Constants.WATCH_ALL_NAMESPACES)
-)
 public class WebsiteReconciler implements Reconciler<Website> {
 
     @Override
     public UpdateControl<Website> reconcile(Website website, Context<Website> context) throws Exception {
         var wrr = context.managedWorkflowAndDependentResourceContext().getWorkflowReconcileResult();
 
-        if (wrr.map(WorkflowReconcileResult::allDependentResourcesReady).orElse(false)) {
+        if (wrr.isPresent() && wrr.get().allDependentResourcesReady()) {
             Log.infof("Site %s is ready", website.getMetadata().getName());
             return UpdateControl
                         .patchStatus(createWebsiteForStatusUpdate(website, true, null));
         }
         else {
-            Log.infof("Site %s not ready yet", website.getMetadata().getName());
+            Log.infof("Site %s is not ready yet", website.getMetadata().getName());
             return UpdateControl
                         .patchStatus(createWebsiteForStatusUpdate(website, false, null))
                         .rescheduleAfter(5, TimeUnit.SECONDS);
@@ -46,7 +39,7 @@ public class WebsiteReconciler implements Reconciler<Website> {
 
     @Override
     public ErrorStatusUpdateControl<Website> updateErrorStatus(Website website, Context<Website> context, Exception e) {
-        Log.error("updateErrorStatus", e);
+        Log.errorf("Error, site %s is not ready: %s", website.getMetadata().getName());
         return ErrorStatusUpdateControl.patchStatus(createWebsiteForStatusUpdate(website, false, e.getMessage()));
     }
 }
